f099dc719682b40a68531587729fde5c75244d38
ChadMar12 <chadmarshall50@gmail.com>
2024-06-20 15:52:16 -0600

    updates

--- Code/Test code/textcode.py
+++ Code/Test code/textcode.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.decomposition import LatentDirichletAllocation
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
@@ -16,7 +17,6 @@ def print_top_words(model, feature_names, n_top_words):
 df = pd.read_csv("dataset.csv")
 
 summary = df['Summary']
-dev = df['Assignee']    # Bug Fixers
 
 x, y = train_test_split(summary, shuffle=True, test_size=0.2, random_state=42)
 
@@ -29,16 +29,24 @@ numTopics = 10
 
 #lda model
 lda = LatentDirichletAllocation(n_components=numTopics, random_state=42)
-trainng = lda.fit(train)
+training = lda.fit(train)
 
 print_top_words(lda, vec.get_feature_names_out(), 10)
 
+trainLda = lda.transform(train)
 testing = lda.transform(test)
 
 #printing topic predictions and the accuracy of it
 for i, topic in enumerate(testing[:10]):
     maxValue = np.max(topic)
     index = np.argmax(topic)
-    print(f"Test Topic #{i} Topic {index} Distrubtion {maxValue}")
+    print(f"Bug Report #{i} Topic {index} Distrubtion {maxValue}")
 
 #Need to run a text classifier on the topics so that I can output the values.......(Thursday)
+
+nbClass = MultinomialNB()
+nbClass.fit(trainLda,x)
+predict = nbClass.predict(testing)
+
+acc = accuracy_score(y, predict)
+print(f'Naive Bayes Score: {acc}')
