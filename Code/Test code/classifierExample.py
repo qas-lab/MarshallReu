fa3a6fcf19215bc57debd4888e5897ca219991e0
ChadMar12 <chadmarshall50@gmail.com>
2024-06-25 01:10:44 -0600

    update

--- Code/Test code/classifierExample.py
+++ Code/Test code/classifierExample.py
@@ -1,11 +1,19 @@
 import pandas as pd
 import numpy as np
 from nltk.corpus import stopwords
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import MultinomialNB, CategoricalNB, ComplementNB
-from sklearn.linear_model import LogisticRegression # See what this is
-from sklearn.metrics import classification_report
+from sklearn.naive_bayes import MultinomialNB, ComplementNB
+from sklearn.neural_network import MLPClassifier #added this to try out
+from sklearn.metrics import classification_report, accuracy_score
+from nltk.stem.wordnet import WordNetLemmatizer     #lemmentizing words
+from nltk.tokenize import word_tokenize             #tokenize words before applying lemmatization
+
+#function for lemmatization (Fix the for loops to what I write)
+def lemmatizeText(text):
+    tokens = word_tokenize(text)
+    lemmatized_tokens = [lemmatizer.lemmatize(token) for token in tokens]
+    return ' '.join(lemmatized_tokens)
 
 stops = list(stopwords.words('english'))
 
@@ -16,29 +24,21 @@ df = pd.read_csv("eclipse_jdt.csv")
 dups = df.dropna(subset=['Duplicated_issue'])
 newData = df.drop(index=dups.index)
 
-text = newData['Description']
-trueLabel = newData['Component']
+#lemmatization
+lemmatizer = WordNetLemmatizer()
 
-# #This is strickly for testing purposes
-# tempDup = df.drop_duplicates(subset=['Component'])
-# labels = list(tempDup['Component'])
-# print(f'The labels are {labels} and the size is {len(labels)}\n')
-
-#data = text.astype(str) + ' ' + trueLabel.astype(str)
+text = newData['Description'].astype(str).apply(lemmatizeText)
+trueLabel = newData['Component'].astype(str).apply(lemmatizeText)
 
 multiVec = MultinomialNB()
-catVec = CategoricalNB()
 compVec = ComplementNB()
+clf = MLPClassifier()
 
-xTrain, xTest, yTrain, yTest = train_test_split(trueLabel.astype(str), text.astype(str), test_size=0.2, random_state=42)
+xTrain, xTest, yTrain, yTest = train_test_split(trueLabel, text, test_size=0.20, random_state=42)
 
 vec = TfidfVectorizer(stop_words=stops)             #Term Freq
 train = vec.fit_transform(yTrain)
 test = vec.transform(yTest)
-
-countVec = CountVectorizer(stop_words=stops)        #Count Vec
-countTrain = countVec.fit_transform(yTrain)
-countTest = countVec.transform(yTest)
 
 #multinomial
 multiVec.fit(train, xTrain)
@@ -46,19 +46,15 @@ predicted = multiVec.predict(test)
 report = classification_report(xTest, predicted)
 print(f'Multinominal Report: \n {report}')
 
-
-
 #complement
 compTrain = compVec.fit(train, xTrain)
 compPredict = compTrain.predict(test)
 compReport = classification_report(xTest, compPredict)
 print(f'Complement Report: \n{compReport}')
 
-
-# NEED TO FINISH THE CATEGORICAL REPORT SO THAT WE CAN COMPARE
-# #categorical
-# catTrain = catVec.fit(countTrain.toarray(), xTrain)
-# catPredict = catVec.predict(countTest.toarray())
-# catReport = classification_report(xTest, catPredict)
-# print(f'Categorical Report: \n {catReport}')
-
+# #mlp 
+# clf.fit(train, xTrain)
+# clfPredict = clf.predict(test)
+# clfReport = classification_report(xTest, clfPredict)
+# clfScroe = accuracy_score(xTest, clfPredict)
+# print(f'CLF Report: \n{clfScroe}')
